08651c37f3fa83c829313c8ecc31b9f2b05cc14d
matsumo-and <yh134.toisanda@gmail.com>
2022-10-10 00:28:42 +0900

    find tree if foundTree is Empty

--- src/track2/App.java
+++ src/track2/App.java
@@ -1,5 +1,6 @@
 package track2;
 
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -7,7 +8,49 @@ public class App {
 
     public static void main(String[] args) {
 
-        String[] lines = getStdin();
+        String[] lines = //getStdin();
+        {
+            "1",
+            "4",
+            "1 1 1 1",
+        };
+
+        final int step = Integer.parseInt(lines[0]);
+        final int days = Integer.parseInt(lines[1]);
+
+        //tree List
+        final String[] treeStrList = lines[2].split(" ");
+        final ArrayList<Integer> treeList = new ArrayList<>();
+        for (String s : treeStrList) {
+            treeList.add(Integer.parseInt(s));
+        }
+
+        //List of found trees
+        final ArrayList<Integer> foundTreeList = new ArrayList<>();
+
+        switch (step){
+            case 1:
+                int fruitNum = 0;
+                for(int currentDay = 0; currentDay < days; currentDay++){
+                    if(foundTreeList.size() == 0){
+                        final int maxTreeNum = Collections.max(treeList);
+                        final int maxTreeIndex = treeList.indexOf(maxTreeNum);
+
+                        foundTreeList.add(maxTreeNum);
+                        treeList.remove(maxTreeIndex);
+                    }else{
+
+                    }
+                }
+                break;
+
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
     }
 
     private static String[] getStdin() {
